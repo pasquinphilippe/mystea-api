c322cd801d81d00fc3461f69bb1af1e3dbcb4adb
Philippe Pasquin <p@pasquin.ca>
2024-03-15 13:16:38 -0400

    no discount and error handlong

--- netlify/functions/requestQuote.js
+++ netlify/functions/requestQuote.js
@@ -23,7 +23,6 @@ exports.handler = async function(event, context) {
     const payload = JSON.parse(event.body);
 
     try {
-        // Prepare the order data
         const orderData = {
             order: {
                 line_items: payload.lineItems.map(item => ({
@@ -38,17 +37,10 @@ exports.handler = async function(event, context) {
                     kind: "authorization",
                     status: "pending",
                     amount: payload.totalPrice
-                }],
-                // Add more fields as necessary, e.g., billing_address, shipping_address
-                discount_codes: [{
-                    code: payload.discountCode, // Assuming this is sent in the payload
-                    amount: payload.discountValue,
-                    type: "fixed_amount" // or "percentage" based on your discount type
                 }]
             }
         };
 
-        // Create the order
         const createOrderResponse = await axios.post(
             `${process.env.SHOPIFY_STORE_URL}/admin/api/2024-01/orders.json`,
             orderData,
@@ -62,16 +54,12 @@ exports.handler = async function(event, context) {
 
         const createdOrderData = createOrderResponse.data;
 
-        // Extract the order status URL
-        const orderStatusUrl = createdOrderData.order.order_status_url;
-
-        // Return the response with the order status URL
         return {
             statusCode: 200,
             headers: headers,
             body: JSON.stringify({
                 order: createdOrderData.order,
-                invoiceUrl: orderStatusUrl
+                invoiceUrl: createdOrderData.order.order_status_url
             })
         };
     } catch (error) {
@@ -83,9 +71,10 @@ exports.handler = async function(event, context) {
         return {
             statusCode: error.response ? error.response.status : 500,
             headers: headers,
-            body: JSON.stringify({ 
+            body: JSON.stringify({
                 error: error.message,
                 response: error.response ? error.response.data : null
             })
         };
     }
+};
